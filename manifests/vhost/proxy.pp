63bcf160fac7f39081f5f666056070d55f68b49b
Adrian Webb <adrian.webb@venturiscm.com>
2012-07-17 03:49:10 -0400

    Updating the proxy server vhost definition to utilize the general vhost definition.

--- manifests/vhost/proxy.pp
+++ manifests/vhost/proxy.pp
@@ -1,53 +1,50 @@
-# Define: apache::vhost::proxy
-#
-# Configures an apache vhost that will only proxy requests
-#
-# Parameters:
-# * $port:
-#     The port on which the vhost will respond
-# * $dest:
-#     URI that the requests will be proxied for
-# - $priority
-# - $template -- the template to use for the vhost
-# - $vhost_name - the name to use for the vhost, defaults to '*'
-#
-# Actions:
-# * Install Apache Virtual Host
-#
-# Requires:
-#
-# Sample Usage:
-#
+
 define apache::vhost::proxy (
-    $port,
-    $dest,
-    $priority      = '10',
-    $template      = 'apache/vhost-proxy.conf.erb',
-    $servername    = '',
-    $serveraliases = '',
-    $ssl           = false,
-    $vhost_name    = '*'
-  ) {
 
-  include apache
+  $server_name         = $name,
+  $aliases             = '',
+  $proxy_url           = '*',
+  $destination         = '',
+  $configure_firewall  = true,
+  $vhost_ip            = $apache::params::vhost_ip,
+  $priority            = $apache::params::priority,
+  $options             = $apache::params::options,
+  $port                = $apache::params::default_port,
+  $use_ssl             = $apache::params::use_ssl,
+  $ssl_cert            = "/etc/ssl/certs/${name}.crt",
+  $ssl_key             = "/etc/ssl/private/${name}.key",
+  $log_dir             = $apache::params::os_apache_log_dir,
+  $error_log_level     = undef,
+  $rewrite_log_level   = undef,
+  $admin_email         = undef,
+  $port_template       = $apache::params::port_template,
+  $vhost_template      = $apache::params::vhost_proxy_template,
 
-  $apache_name = $apache::params::apache_name
-  $ssl_path = $apache::params::ssl_path
-  $srvname = $name
+) {
 
-  if $ssl == true {
-    include apache::ssl
+  #-----------------------------------------------------------------------------
+
+  apache::vhost { "${priority}-${server_name}-proxy":
+    server_name         => $server_name,
+    aliases             => $aliases,
+    doc_root            => undef,
+    configure_firewall  => $configure_firewall,
+    vhost_ip            => $vhost_ip,
+    priority            => $priority,
+    options             => $options,
+    port                => $port,
+    use_ssl             => $use_ssl,
+    ssl_cert            => $ssl_cert,
+    ssl_key             => $ssl_key,
+    log_dir             => $log_dir,
+    error_log_level     => $error_log_level,
+    rewrite_log_level   => $rewrite_log_level,
+    admin_email         => $admin_email,
+    extra               => {
+      'proxy_url'         => $proxy_url,
+      'destination'       => $destination,
+    },
+    port_template       => $port_template,
+    vhost_template      => $vhost_template,
   }
-
-  file { "${priority}-${name}":
-    path    => "${apache::params::vdir}/${priority}-${name}",
-    content => template($template),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    require => Package['httpd'],
-    notify  => Service['httpd'],
-  }
-
-
 }
