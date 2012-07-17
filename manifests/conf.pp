c8fa1cf731c8852a11d1a6bb1dddcf10e00721df
Adrian Webb <adrian.webb@venturiscm.com>
2012-07-17 03:26:27 -0400

    Fixing settings in the conf definition.

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -9,21 +9,21 @@ define apache::conf (
 
   #-----------------------------------------------------------------------------
 
-  $config_path = "${apache::params::apache_etc}/conf.d/${name}.conf"
+  $config_file = "${apache::params::os_apache_conf_dir}/${name}.conf"
 
   File {
     ensure  => $ensure,
     owner   => 'root',
     group   => 'root',
     mode    => 644,
-    require => File['apache_vdir'],
+    require => File['apache_conf_dir'],
     notify  => Service['apache'],
   }
 
   if $content {
-    file { $config_path: content => $content, }
+    file { $config_file: content => $content, }
   }
   elsif $source {
-    file { $config_path: source => $source, }
+    file { $config_file: source => $source, }
   }
 }
