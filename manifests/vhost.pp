78ea0c5d106460b30aaa6f92e3612bb72b5513ac
Adrian Webb <adrian.webb@venturiscm.com>
2012-07-17 03:41:31 -0400

    Refactoring the general purpose vhost definition to make it more friendly for sub classes.

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -30,97 +30,63 @@
 #
 define apache::vhost (
 
-  $server_name        = $name,
-  $aliases            = '',
-  $doc_root           = "${apache::params::web_home}/${name}",
-  $configure_firewall = true,
-  $vhost_ip           = $apache::params::vhost_ip,
-  $priority           = $apache::params::priority,
-  $options            = $apache::params::options,
-  $http_port          = $apache::params::http_port,
-  $use_ssl            = $apache::params::use_ssl,
-  $ssl_cert           = "/etc/ssl/certs/${name}.crt",
-  $ssl_key            = "/etc/ssl/private/${name}.key",
-  $https_port         = $apache::params::https_port,
-  $log_dir            = $apache::params::apache_log_dir,
-  $error_log_level    = undef,
-  $rewrite_log_level  = undef,
-  $admin_email        = undef,
-  $http_template      = $apache::params::http_template,
-  $https_template     = $apache::params::https_template,
+  $server_name         = $name,
+  $aliases             = '',
+  $doc_root            = "${apache::params::web_home}/${name}",
+  $configure_firewall  = true,
+  $vhost_ip            = $apache::params::vhost_ip,
+  $priority            = $apache::params::priority,
+  $options             = $apache::params::options,
+  $port                = $apache::params::default_port,
+  $use_ssl             = $apache::params::use_ssl,
+  $ssl_cert            = "/etc/ssl/certs/${name}.crt",
+  $ssl_key             = "/etc/ssl/private/${name}.key",
+  $log_dir             = $apache::params::os_apache_log_dir,
+  $error_log_level     = undef,
+  $rewrite_log_level   = undef,
+  $admin_email         = undef,
+  $extra               = {},
+  $port_template       = $apache::params::port_template,
+  $vhost_template      = $apache::params::vhost_template,
 
 ) {
 
   include apache
 
-  if $http_port {
-    file { "${priority}-${server_name}.http.conf":
-      path    => "${apache::params::apache_vdir}/${priority}-${server_name}.http.conf",
-      content => template($http_template),
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755',
-      require => Package['apache'],
-      notify  => Service['apache'],
-    }
-
-    file { "${priority}-${server_name}-http_port":
-      path => "${apache::params::apache_ports_dir}/ports.${http_port}.conf",
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => template('apache/ports.conf.erb'),
-      require => Package['apache'],
-      notify  => Service['apache'],
-    }
-
-    if $configure_firewall {
-      if ! defined(Firewall["0500 INPUT Accept Apache connection: $http_port"]) {
-        firewall {
-          "0500 INPUT Accept Apache connection: $http_port":
-            action    => 'accept',
-            dport     => $http_port,
-            proto     => 'tcp',
-            subscribe => File["${priority}-${server_name}-http_port"],
-        }
-      }
-    }
+  if $use_ssl {
+    include apache::mod::ssl
   }
 
-  if $use_ssl and $https_port and $ssl_cert and $ssl_key {
-    include apache::ssl
+  #-----------------------------------------------------------------------------
 
-    file { "${priority}-${server_name}.https.conf":
-      path    => "${apache::params::apache_vdir}/${priority}-${server_name}.https.conf",
-      content => template($https_template),
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755',
-      require => Package['apache'],
+  if $port {
+    file { "${apache::params::os_apache_vhost_dir}/${server_name}.conf":
+      content => template($vhost_template),
+      require => File['apache_vhost_dir'],
       notify  => Service['apache'],
     }
 
-    file { "${priority}-${server_name}-https_port":
-      path => "${apache::params::apache_ports_dir}/ports.ssl.${https_port}.conf",
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => template('apache/ports.ssl.conf.erb'),
-      require => Package['apache'],
-      notify  => Service['apache'],
+    $port_config_file = "${apache::params::os_apache_conf_dir}/ports.${port}.conf"
+
+    if ! defined(File[$port_config_file]) {
+      file { $port_config_file:
+        content => template($port_template),
+        require => File['apache_conf_dir'],
+        notify  => Service['apache'],
+      }
     }
 
     if $configure_firewall {
-      if ! defined(Firewall["0550 INPUT Accept Apache SSL connection: $https_port"]) {
-        firewall {
-          "0550 INPUT Accept Apache connection: $https_port":
+      $firewall_description = "0500 INPUT Accept Apache connection: $port"
+
+      if ! defined(Firewall[$firewall_description]) {
+        firewall {$firewall_description:
             action    => 'accept',
-            dport     => $https_port,
+            dport     => $port,
             proto     => 'tcp',
-            subscribe => File["${priority}-${server_name}-https_port"],
+            subscribe => File[$port_config_file],
         }
       }
     }
   }
 }
-
